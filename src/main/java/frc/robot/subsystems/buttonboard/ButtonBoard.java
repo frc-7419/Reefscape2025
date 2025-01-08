e853616ae0bc236c989bc123efc1aa7b492fa046
Ary <ary10kum@gmail.com>
2025-01-08 13:17:46 -0800

    Button For Loop

--- src/main/java/frc/robot/subsystems/buttonboard/ButtonBoard.java
+++ src/main/java/frc/robot/subsystems/buttonboard/ButtonBoard.java
@@ -17,25 +17,13 @@ public class ButtonBoard extends SubsystemBase {
   @Override
   public void periodic() {
     SmartDashboard.putString("Button States: ", "0 for not pressed 1 for pressed");
-    SmartDashboard.putBoolean("Button 0 Pressed: ", buttonBoard.getRawButton(0));
-    SmartDashboard.putBoolean("Button 1 Pressed: ", buttonBoard.getRawButton(1));
-    SmartDashboard.putBoolean("Button 2 Pressed: ", buttonBoard.getRawButton(2));
-    SmartDashboard.putBoolean("Button 3 Pressed: ", buttonBoard.getRawButton(3));
-    SmartDashboard.putBoolean("Button 4 Pressed: ", buttonBoard.getRawButton(4));
-    SmartDashboard.putBoolean("Button 5 Pressed: ", buttonBoard.getRawButton(5));
-    SmartDashboard.putBoolean("Button 6 Pressed: ", buttonBoard.getRawButton(6));
-    SmartDashboard.putBoolean("Button 7 Pressed: ", buttonBoard.getRawButton(7));
-    SmartDashboard.putBoolean("Button 8 Pressed: ", buttonBoard.getRawButton(9));
-    SmartDashboard.putBoolean("Button 9 Pressed: ", buttonBoard.getRawButton(9));
-    SmartDashboard.putBoolean("Button 10 Pressed: ", buttonBoard.getRawButton(10));
-    SmartDashboard.putBoolean("Button 11 Pressed: ", buttonBoard.getRawButton(11));
-    SmartDashboard.putBoolean("Button 12 Pressed: ", buttonBoard.getRawButton(12));
-    SmartDashboard.putBoolean("Button 13 Pressed: ", buttonBoard.getRawButton(13));
-    SmartDashboard.putBoolean("Button 14 Pressed: ", buttonBoard.getRawButton(14));
-    SmartDashboard.putBoolean("Button 15 Pressed: ", buttonBoard.getRawButton(15));
+    for (int i = 0; i < 12; i++) {
+      SmartDashboard.putBoolean("Button " + i + " Pressed: ", buttonBoard.getRawButton(i));
+    }
     SmartDashboard.putNumber("Left joystick X axis", controller.getLeftX());
     SmartDashboard.putNumber("Left joystick Y axis", controller.getLeftY());
     SmartDashboard.putNumber("Right joystick X axis", controller.getRightX());
     SmartDashboard.putNumber("Right joystick Y axis", controller.getRightY());
   }
 }
+
