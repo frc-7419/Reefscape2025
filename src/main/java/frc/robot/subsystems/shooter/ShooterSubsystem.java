b5f6d60fff66e0d68aac6dd6a8cdde210c9beab7
Ary <ary10kum@gmail.com>
2025-01-08 14:14:07 -0800

    Code Hierarchy (theoretical) finish
    Co-Authored-By: Brian Lam <110221313+brainlame@users.noreply.github.com>
    Co-Authored-By: rqhu2025 <91698182+rqhu2025@users.noreply.github.com>
    Co-Authored-By: Ethan <179424948+Bachthoven@users.noreply.github.com>
    Co-Authored-By: CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
    Co-Authored-By: Eshan41 <179530953+Eshan41@users.noreply.github.com>
    Co-Authored-By: Yashveer1234 <141895202+Yashveer1234@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+  /** Creates a new IntakeSubsystem. */
+  public ShooterSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
