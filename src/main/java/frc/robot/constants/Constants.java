da01d931278f738adbc5bc451be322570f6a233f
Max <maxshi239@gmail.com>
2025-01-10 15:34:34 -0800

    updated deviation constants

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -20,6 +20,6 @@ public class Constants {
     public static final AngularVelocity kMaxAngularRate = RotationsPerSecond.of(0.75);
   }
 
-  public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+  public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(3, 5, 7);
   public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
 }
