8cc0c6679282efaaa5b569dbecda00a826234dad
Team7419 <qls7419leadership@gmail.com>
2025-01-14 14:01:10 -0800

    Revert "nothing much"
    This reverts commit af80da64470e5eae68863728b5a5e90f477c82dc.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,18 +4,74 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.numbers.N0;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.CANBus.CANBusStatus;
+import edu.wpi.first.hal.can.CANStatus;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.constants.Constants.RobotConstants;
+import frc.robot.util.CombinedAlert;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
+
+  private final CombinedAlert canErrorAlert =
+      new CombinedAlert(
+          CombinedAlert.Severity.ERROR,
+          "RIO CAN errors detected.",
+          "The RIO CAN bus is not responding properly.");
+  private final CombinedAlert canivoreErrorAlert =
+      new CombinedAlert(
+          CombinedAlert.Severity.ERROR,
+          "CANivore error detected.",
+          "The CANivore device is not responding properly.");
+  private final CombinedAlert lowBatteryAlert =
+      new CombinedAlert(
+          CombinedAlert.Severity.WARNING,
+          "Low Battery / Brownout",
+          "Voltage is under " + RobotConstants.kLowBatteryVoltage + "V.");
+  CANBus canivore = new CANBus(RobotConstants.kCANivoreBus);
+
+  private void updateRobotStatus() {
+    CANStatus rioCanStatus = RobotController.getCANStatus();
+    CANBusStatus canivoreStatus = canivore.getStatus();
+
+    SmartDashboard.putNumber(
+        "RIO CAN Bus Utilization (%)", rioCanStatus.percentBusUtilization * 100);
+    SmartDashboard.putNumber("RIO CAN Bus Off Count", rioCanStatus.busOffCount);
+    SmartDashboard.putNumber("RIO CAN TX Full Count", rioCanStatus.txFullCount);
+    SmartDashboard.putNumber("RIO CAN Receive Error Count", rioCanStatus.receiveErrorCount);
+    SmartDashboard.putNumber("RIO CAN Transmit Error Count", rioCanStatus.transmitErrorCount);
+
+    SmartDashboard.putNumber(
+        "CANivore CAN Bus Utilization (%)", canivoreStatus.BusUtilization * 100);
+    SmartDashboard.putNumber("CANivore CAN Bus Off Count", canivoreStatus.BusOffCount);
+    SmartDashboard.putNumber("CANivore CAN TX Full Count", canivoreStatus.TxFullCount);
+    SmartDashboard.putString("CANivore CAN Status", canivoreStatus.Status.getName());
+
+    SmartDashboard.putNumber("Battery Voltage", RobotController.getBatteryVoltage());
+
+    if (rioCanStatus.receiveErrorCount > 0 || rioCanStatus.transmitErrorCount > 0) {
+      canErrorAlert.set(true);
+    } else {
+      canErrorAlert.set(false);
+    }
+    if (canivoreStatus.Status.isError()) {
+      canivoreErrorAlert.set(true);
+    } else {
+      canivoreErrorAlert.set(false);
+    }
+    if (RobotController.getBatteryVoltage() <= RobotConstants.kLowBatteryVoltage) {
+      lowBatteryAlert.set(true);
+    } else {
+      lowBatteryAlert.set(false);
+    }
+  }
 
   public Robot() {
     m_robotContainer = new RobotContainer();
@@ -24,17 +80,16 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-            // Correct pose estimate with vision measurements
-            var visionEst = m_robotContainer.visionSubsystem.getEstimatedGlobalPose();
-            visionEst.ifPresent(
-                    est -> {
-                        // Change our trust in the measurement based on the tags we can see
-                        var estStdDevs = m_robotContainer.visionSubsystem.getEstimationStdDevs();
-                        // Matrix<N3, N1> estStdDevs = new Matrix<N3, N1>(N3.instance, N1.instance);
-                        m_robotContainer.drivetrain.resetPose(est.estimatedPose.toPose2d());
-                        // m_robotContainer.drivetrain.addVisionMeasurement(
-                        //         est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs);
-                    });
+    var visionEst = m_robotContainer.photonvision.getEstimatedGlobalPose();
+    visionEst.ifPresent(
+        est -> {
+          // Change our trust in the measurement based on the tags we can see
+          var estStdDevs = m_robotContainer.photonvision.getEstimationStdDevs();
+
+          m_robotContainer.drivetrain.addVisionMeasurement(
+              est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs);
+        });
+    updateRobotStatus();
   }
 
   @Override
---
+++ src/main/java/frc/robot/commands/ToPose.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
+import java.util.Optional;
+
+public class ToPose extends SequentialCommandGroup {
+  private Pose2d targetPose;
+
+  public ToPose(CommandSwerveDrivetrain drivetrain) {
+    Optional<Alliance> alliance = DriverStation.getAlliance();
+
+    SmartDashboard.putBoolean("alliance present", alliance.isPresent());
+    targetPose = new Pose2d(3.812, 5.392, Rotation2d.fromDegrees(180));
+
+    PathConstraints pathConstraints =
+        new PathConstraints(3, 4, Units.degreesToRadians(540), Units.degreesToRadians(720));
+
+    Command pathfindingCommand = AutoBuilder.pathfindToPose(targetPose, pathConstraints, 0);
+
+    addCommands(pathfindingCommand);
+  }
+}
