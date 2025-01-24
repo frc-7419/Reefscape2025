01e3fbe949cc2196e6544c52439c3f4a108bdf34
CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
2025-01-24 09:58:30 -0800

    Made the runHanger.java with human and robot hanging

--- src/main/java/frc/robot/commands/RunHanger.java
+++ src/main/java/frc/robot/commands/RunHanger.java
@@ -4,37 +4,50 @@
 
 package frc.robot.commands;
 
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
-import frc.robot.subsystems.elevator.ElevatorToSetpoint;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class RunHanger extends Command {
   /** Creates a new RunHanger. */
+  
   private ElevatorSubsystem elevatorSubsystem;
-  private ElevatorToSetpoint elevatorToSetpoint;
+  private boolean humanInterference = false; // Human hanging or auto hanging
+  private CommandXboxController xboxController;
 
-  public RunHanger(ElevatorSubsystem elevatorSubsystem, ElevatorToSetpoint elevatorToSetpoint) {
-    this.elevatorToSetpoint = elevatorToSetpoint;
+  public RunHanger(ElevatorSubsystem elevatorSubsystem, boolean humanInterference, CommandXboxController xboxController) {
+    this.xboxController = xboxController;
+    this.humanInterference = humanInterference;
     this.elevatorSubsystem = elevatorSubsystem;
     addRequirements(elevatorSubsystem);
     // Use addRequirements() here to declare subsystem dependencies.
   }
-  }
+  
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    elevatorToSetpoint.setPos(0.0);
+    elevatorSubsystem.coast();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (humanInterference) {
+      elevatorSubsystem.setPower(xboxController.getLeftY()); // Arbitrary joystick input
+    }
+    else {
+      elevatorSubsystem.setPower(-0.5);
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    elevatorSubsystem.brake();
+  }
 
   // Returns true when the command should end.
   @Override
