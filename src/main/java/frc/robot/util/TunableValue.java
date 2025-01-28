938334cbcd8c73d69927fb205c8df04ecbd34f35
Sepandar <sepandar24@gmail.com>
2025-01-28 15:53:52 -0800

    applied spotless

--- src/main/java/frc/robot/util/TunableValue.java
+++ src/main/java/frc/robot/util/TunableValue.java
@@ -3,17 +3,17 @@ package frc.robot.util;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TunableValue {
-    private double value;
-    private String name;
+  private double value;
+  private String name;
 
-    public TunableValue(String name, double defaultValue) {
-        this.value = defaultValue;
-        this.name = name;
-        SmartDashboard.putNumber(name, defaultValue);
-    }
+  public TunableValue(String name, double defaultValue) {
+    this.value = defaultValue;
+    this.name = name;
+    SmartDashboard.putNumber(name, defaultValue);
+  }
 
-    public double getValue() {
-        this.value = SmartDashboard.getNumber(this.name, this.value);
-        return this.value;
-    }
+  public double getValue() {
+    this.value = SmartDashboard.getNumber(this.name, this.value);
+    return this.value;
+  }
 }
