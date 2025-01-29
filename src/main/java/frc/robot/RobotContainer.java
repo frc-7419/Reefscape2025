f80d21a370bfa600500dfa9922dc3f1d345e56d5
rqhu2025 <rhustudy@gmail.com>
2025-01-29 11:08:27 -0800

    Revert "Merge branch 'allpaths' of https://github.com/frc-7419/Reefscape2025 into allpaths"
    This reverts commit 3a308d7aafb7d9d148aec4f1dfd2aaa82d96e138, reversing
    changes made to b2a055e66b8b9b97ab5b148a63d18c8fc474c7ae.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,73 +7,130 @@ package frc.robot;
 import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
-import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
-import frc.robot.subsystems.Vision;
+import frc.robot.commands.AntiTip;
+import frc.robot.commands.ToPose;
+import frc.robot.constants.Constants.CameraConfig;
+import frc.robot.constants.Constants.DrivetrainConstants;
+import frc.robot.constants.Constants.VisionConstants;
+import frc.robot.constants.TunerConstants;
+import frc.robot.subsystems.PhotonvisionSubsystem;
+import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
+import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RobotContainer {
-    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+  private double MaxSpeed = DrivetrainConstants.kMaxVelocity.in(MetersPerSecond);
+  private double MaxAngularRate = DrivetrainConstants.kMaxAngularRate.in(RotationsPerSecond);
 
-    /* Setting up bindings for necessary control of the swerve drive platform */
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+  private final CommandXboxController driver = new CommandXboxController(0);
+  private final CommandXboxController operator = new CommandXboxController(1);
 
-    private final Telemetry logger = new Telemetry(MaxSpeed);
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final SwerveRequest.FieldCentric drive =
+      new SwerveRequest.FieldCentric()
+          .withDeadband(MaxSpeed * 0.1)
+          .withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+          .withDriveRequestType(
+              DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+  private final SwerveRequest.RobotCentric forwardStraight =
+      new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
+  private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+  public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+  private final ToPose toPose = new ToPose(drivetrain);
+  // private final SendableChooser<Command> autoChooser;
+  private final ElevatorSubsystem elevator = new ElevatorSubsystem();
+  private final AntiTip antiTip = new AntiTip(drivetrain, elevator);
+  public final PhotonvisionSubsystem photonvision;
+  private final CameraConfig photonCamOne =
+      new CameraConfig("Photon_Vision_Cam_1", VisionConstants.kRobotToCamOne);
+  private final CameraConfig photonCamTwo =
+      new CameraConfig("Photon_Vision_Cam_2", VisionConstants.kRobotToCamOne);
+  private final List<CameraConfig> cameraConfigs =
+      new ArrayList<CameraConfig>() {
+        {
+          add(photonCamOne);
+          add(photonCamTwo);
+        }
+      };
 
-    public final Vision visionSubsystem = new Vision();
+  public RobotContainer() {
+    photonvision = new PhotonvisionSubsystem(cameraConfigs);
 
-    public RobotContainer() {
-        configureBindings();
-    }
+    configureBindings();
+    SmartDashboard.putBoolean("isConfigured", AutoBuilder.isConfigured());
+    // antiTip.schedule();
+  }
 
-    private void configureBindings() {
-        // Note that X is defined as forward according to WPILib convention,
-        // and Y is defined as to the left according to WPILib convention.
-        drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
-        );
+  private void configureBindings() {
+    // Note that X is defined as forward according to WPILib convention,
+    // and Y is defined as to the left according to WPILib convention.
+    drivetrain.setDefaultCommand(
+        // Drivetrain will execute this command periodically
+        drivetrain.applyRequest(
+            () ->
+                drive
+                    .withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with
+                    // negative Y (forward)
+                    .withVelocityY(
+                        -driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(
+                        -driver.getRightX()
+                            * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            ));
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+    driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    driver
+        .b()
+        .whileTrue(
+            drivetrain.applyRequest(
+                () ->
+                    point.withModuleDirection(
+                        new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+    // driver
+    // .pov(0)
+    // .whileTrue(
+    // drivetrain.applyRequest(() ->
+    // forwardStraight.withVelocityX(0.5).withVelocityY(0)));
+    // driver
+    // .pov(180)
+    // .whileTrue(
+    // drivetrain.applyRequest(() ->
+    // forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
+    driver.x().whileTrue(toPose);
 
-        // reset the field-centric heading on left bumper press
-        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+    // Run SysId routines when holding back/start and X/Y.
+    // Note that each routine should be run exactly once in a single log.
+    driver.back().and(driver.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+    driver.back().and(driver.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+    driver.start().and(driver.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+    driver.start().and(driver.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-        drivetrain.registerTelemetry(logger::telemeterize);
-    }
+    // reset the field-centric heading on left bumper press
+    driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
-    public Command getAutonomousCommand() {
-        return new PathPlannerAuto("New Auto");
-    }
+    drivetrain.registerTelemetry(logger::telemeterize);
+
+    operator.a().whileTrue(elevator.setPosition(Inches.of(0)));
+
+    elevator.setDefaultCommand(elevator.joystickControl(operator.getRightY()));
+  }
+
+  public Command getAutonomousCommand() {
+    /* First put the drivetrain into auto run mode, then run the auto */
+    return new PathPlannerAuto("Example Path");
+  }
 }
