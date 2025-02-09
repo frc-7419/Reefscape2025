4c89ba20fc4d0357e1df9f24e587be630d565407
Amrit-s-RIS <therealamritsingh@gmail.com>
2025-02-08 17:18:19 -0800

    this one guy named amrit forgot to run spotless before the pr. what a buffoon!

--- src/main/java/frc/robot/subsystems/algaeintake/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/algaeintake/AlgaeIntakeSubsystem.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.DigitalInput;
