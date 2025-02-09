a9fcd410b29f3e31498c0ff9d5d5399ba17f4d74
Amrit-s-RIS <therealamritsingh@gmail.com>
2025-02-08 17:15:55 -0800

    what it do clue crew

--- src/main/java/frc/robot/commands/RunAlgaeIntakeWithJoystick.java
+++ src/main/java/frc/robot/commands/RunAlgaeIntakeWithJoystick.java
@@ -5,25 +5,38 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.algaeintake.AlgaeIntakeSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class RunAlgaeIntakeWithJoystick extends Command {
-  /** Creates a new RunAlgaeIntakeWithJoystick. */
-  public RunAlgaeIntakeWithJoystick() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private final CommandXboxController joystick;
+  private final AlgaeIntakeSubsystem algaeIntakeSubsystem;
+
+  public RunAlgaeIntakeWithJoystick(
+      CommandXboxController joystick, AlgaeIntakeSubsystem algaeIntakeSubsystem) {
+    this.joystick = joystick;
+    this.algaeIntakeSubsystem = algaeIntakeSubsystem;
+    addRequirements(algaeIntakeSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    algaeIntakeSubsystem.coast();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    algaeIntakeSubsystem.setPower(joystick.getLeftX());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    algaeIntakeSubsystem.brake();
+  }
 
   // Returns true when the command should end.
   @Override
