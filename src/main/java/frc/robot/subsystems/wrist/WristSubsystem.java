9bedd4f949026068c20ad03fa6761cd93cfe233c
ds <ds@localhost.local>
2025-02-21 17:23:27 -0800

    fix comments

--- src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.wrist;
 import static edu.wpi.first.units.Units.Celsius;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
@@ -10,11 +11,13 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.constants.Constants.RobotConstants;
 import frc.robot.constants.Constants.WristConstants;
 import frc.robot.util.CombinedAlert;
 
@@ -143,9 +146,9 @@ public class WristSubsystem extends SubsystemBase {
     return Rotations.of(wristEncoder.get());
   }
 
-  // public AngularVelocity getVelocity() {
-  //   return wristEncoder.getVelocity().getValue();
-  // }
+  public AngularVelocity getVelocity() {
+    return wristMotor.getVelocity().getValue();
+  }
 
   @Override
   public void periodic() {
@@ -167,34 +170,34 @@ public class WristSubsystem extends SubsystemBase {
    * @return {@code true} if the wrist passes all safety checks and is operating safely, {@code
    *     false} otherwise.
    */
-  // private boolean safetyCheck() {
-  //   if (!RobotConstants.runSafetyCheck) return true;
+  private boolean safetyCheck() {
+    if (!RobotConstants.runSafetyCheck) return true;
 
-  //   if (getVelocity().abs(RotationsPerSecond)
-  //       >= WristConstants.UNSAFE_SPEED.in(RotationsPerSecond)) {
-  //     brake();
-  //     velocityAlert.set(true);
-  //     return false;
-  //   } else velocityAlert.set(false);
+    if (getVelocity().abs(RotationsPerSecond)
+        >= WristConstants.UNSAFE_SPEED.in(RotationsPerSecond)) {
+      brake();
+      velocityAlert.set(true);
+      return false;
+    } else velocityAlert.set(false);
 
-  //   if (wristMotor.getDeviceTemp().getValue().gte(WristConstants.MAX_TEMPERATURE)) {
-  //     brake();
-  //     overheatingAlert.set(true);
-  //     return false;
-  //   } else {
-  //     overheatingAlert.set(false);
-  //   }
+    if (wristMotor.getDeviceTemp().getValue().gte(WristConstants.MAX_TEMPERATURE)) {
+      brake();
+      overheatingAlert.set(true);
+      return false;
+    } else {
+      overheatingAlert.set(false);
+    }
 
-  //   Angle currentAngle = getPosition();
-  //   if (currentAngle.gt(WristConstants.kMaxAngle.plus(WristConstants.kAngleTolerance))
-  //       || currentAngle.lt(WristConstants.kMinAngle.minus(WristConstants.kAngleTolerance))) {
-  //     brake();
-  //     positionAlert.set(true);
-  //     return false;
-  //   } else {
-  //     positionAlert.set(false);
-  //     coast();
-  //   }
-  //   return true;
-  // }
+    Angle currentAngle = getPosition();
+    if (currentAngle.gt(WristConstants.kMaxAngle.plus(WristConstants.kAngleTolerance))
+        || currentAngle.lt(WristConstants.kMinAngle.minus(WristConstants.kAngleTolerance))) {
+      brake();
+      positionAlert.set(true);
+      return false;
+    } else {
+      positionAlert.set(false);
+      coast();
+    }
+    return true;
+  }
 }
