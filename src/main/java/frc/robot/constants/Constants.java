487c1189542eaaa43648022709d39584d4483d0e
Team7419 <qls7419leadership@gmail.com>
2025-03-07 11:01:22 -0800

    vision offsets

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -140,9 +140,9 @@ public class Constants {
     public static final Transform3d kRobotToCamOne = new Transform3d(
         new Translation3d(
             Units.inchesToMeters(11.375),
-            Units.inchesToMeters(11.375),
+            Units.inchesToMeters(-11.375),
             Units.inchesToMeters(9)),
-        new Rotation3d(0, Units.degreesToRadians(-28.125), Units.degreesToRadians(90)));
+        new Rotation3d(0, Units.degreesToRadians(-28.125), Units.degreesToRadians(-90)));
     public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(3, 5, 7);
     public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
     public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout
@@ -150,7 +150,7 @@ public class Constants {
     public static final Transform3d kRobotToCamTwo = new Transform3d(
         new Translation3d(
             Units.inchesToMeters(11.375),
-            Units.inchesToMeters(-11.375),
+            Units.inchesToMeters(11.375),
             Units.inchesToMeters(9)),
         new Rotation3d(0, Units.degreesToRadians(-28.125), Units.degreesToRadians(0)));
   }
