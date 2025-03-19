8cf8f1b29c030ea92025932d96cd70eb473e41f1
MostOriginalIGN <winhtetlin0001@gmail.com>
2025-03-19 11:22:29 -0700

    align to color

---
+++ src/main/java/frc/robot/commands/AlignToColor.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.Constants.DrivetrainConstants;
+import frc.robot.subsystems.ColorDetectionSubsystem;
+import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class AlignToColor extends Command {
+  private final CommandSwerveDrivetrain drivetrain;
+  private final ColorDetectionSubsystem colorDetectionSubsystem;
+  private final ProfiledPIDController pid = DrivetrainConstants.kPoseVelocityYController;
+
+  private final SwerveRequest.RobotCentric drive =
+      new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+
+  public AlignToColor(
+      CommandSwerveDrivetrain drivetrain, ColorDetectionSubsystem colorDetectionSubsystem) {
+    this.drivetrain = drivetrain;
+    this.colorDetectionSubsystem = colorDetectionSubsystem;
+    addRequirements(drivetrain, colorDetectionSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+    TrapezoidProfile.Constraints velocityConstraints = new TrapezoidProfile.Constraints(1, 1);
+    pid.setConstraints(velocityConstraints);
+    pid.setGoal(0);
+
+    pid.reset(colorDetectionSubsystem.getTXNC());
+    pid.setTolerance(5);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double error = colorDetectionSubsystem.getTXNC();
+    double output = pid.calculate(error);
+
+    drivetrain.setControl(drive.withVelocityY(output));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivetrain.setControl(drive.withVelocityY(0));
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return pid.atGoal();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ColorDetectionSubsystem.java
@@ -0,0 +1,65 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
+
+public class ColorDetectionSubsystem extends SubsystemBase {
+  private final String limelight;
+  private static final double ALIGN_THRESHOLD = 0.1;
+  private static final double MIN_TARGET_AREA = 10.0;
+
+  public ColorDetectionSubsystem(String limelightName) {
+    this.limelight = limelightName;
+    LimelightHelpers.setLEDMode_ForceOff(limelight);
+  }
+
+  public double getTXNC() {
+    return LimelightHelpers.getTXNC(limelight);
+  }
+
+  public double getTargetArea() {
+    return LimelightHelpers.getTA(limelight);
+  }
+
+  public boolean hasValidTarget() {
+    return getTargetArea() > MIN_TARGET_AREA;
+  }
+
+  public boolean isAligned() {
+    if (!hasValidTarget()) {
+      return false;
+    }
+    return Math.abs(getTXNC()) < ALIGN_THRESHOLD;
+  }
+
+  public void enableLED() {
+    LimelightHelpers.setLEDMode_ForceOn(limelight);
+  }
+
+  public void disableLED() {
+    LimelightHelpers.setLEDMode_ForceOff(limelight);
+  }
+
+  public void blinkLED() {
+    LimelightHelpers.setLEDMode_ForceBlink(limelight);
+  }
+
+  public void setLEDModePipelineControl() {
+    LimelightHelpers.setLEDMode_PipelineControl(limelight);
+  }
+
+  public void setPipeline(int pipelineIndex) {
+    LimelightHelpers.setPipelineIndex(limelight, pipelineIndex);
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Reef TXNC", getTXNC());
+    SmartDashboard.putBoolean("Is Aligned (Color)", isAligned());
+  }
+}
